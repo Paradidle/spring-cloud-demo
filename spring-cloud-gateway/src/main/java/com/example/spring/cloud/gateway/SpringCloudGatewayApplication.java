68e202bd1abc66bc68161e963146243460939ca6
chenyupeng <306592662@qq.com>
2020-05-17 23:39:02 +0800

    补充安全校验账号密码

--- spring-cloud-gateway/src/main/java/com/example/spring/cloud/gateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/example/spring/cloud/gateway/SpringCloudGatewayApplication.java
@@ -3,6 +3,8 @@ package com.example.spring.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
