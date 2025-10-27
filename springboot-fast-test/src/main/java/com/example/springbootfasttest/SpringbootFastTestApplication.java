0b45cce71ade4c34576230656620959d6bec4f31
Paradidle <306592662@qq.com>
2025-10-27 18:43:28 +0800

    fast test

---
+++ springboot-fast-test/src/main/java/com/example/springbootfasttest/SpringbootFastTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootfasttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFastTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFastTestApplication.class, args);
+    }
+
+}
---
+++ springboot-fast-test/src/main/java/com/example/springbootfasttest/controller/TestController.java
@@ -0,0 +1,34 @@
+package com.example.springbootfasttest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * <p>
+ * Copyright: 2025 . All rights reserved.
+ * </p>
+ * <p>
+ * Company: Zsoft
+ * </p>
+ * <p>
+ * CreateDate:2025/10/27
+ * </p>
+ *
+ * @author chenyupeng
+ * @history Mender:chenyupeng；Date:2025/10/27；
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello world";
+    }
+
+}
