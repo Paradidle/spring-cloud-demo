2dc8bacef1b19f5a97ef2e300de6bf3b32014b1f
Paradidle <306592662@qq.com>
2025-12-13 17:44:51 +0800

    spring fast test

---
+++ springboot-fast-test/src/main/java/com/example/springbootfasttest/result/CustomHttpServletRequestWrapper.java
@@ -0,0 +1,52 @@
+package com.example.springbootfasttest.result;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import org.apache.commons.io.IOUtils;
+
+public class CustomHttpServletRequestWrapper extends HttpServletRequestWrapper {
+    private final byte[] body;
+
+    public CustomHttpServletRequestWrapper(HttpServletRequest request) throws IOException {
+        super(request);
+        // 读取请求体并缓存
+        body = IOUtils.toByteArray(request.getInputStream());
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(body);
+        return new ServletInputStream() {
+            @Override
+            public boolean isFinished() {
+                return byteArrayInputStream.available() == 0;
+            }
+
+            @Override
+            public boolean isReady() {
+                return true;
+            }
+
+            @Override
+            public void setReadListener(ReadListener readListener) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public int read() throws IOException {
+                return byteArrayInputStream.read();
+            }
+        };
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(this.getInputStream()));
+    }
+}
